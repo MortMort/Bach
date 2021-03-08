e2a08a8a1e3879e8de4ee9aeec6eb525a12a9139
Martin Højlund Therkildsen <Martin.therkildsen@gmail.com>
2021-03-08 13:54:38 +0100

    Added maf_filter.m - Will become documentation for the MAF filter

---
+++ Simulink/learning/maf_filter.m
@@ -0,0 +1,20 @@
+clc; clear; close all;
+
+
+fs = 1000;
+Ts = 1/fs;
+T_MAF = 0.02;
+N = T_MAF/Ts;
+
+% transfer function based on: short definition:
+z = tf('z', Ts)
+H_MAF = 1/N * (1-z^-N)/(1-z^-1)
+
+figure
+bode(H_MAF)
+
+% transfer function based on: filter koefficients
+a = 1;
+b = 1/N*ones(N,1);
+figure
+freqz(b,a,3000)
