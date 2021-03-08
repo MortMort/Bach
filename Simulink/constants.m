8e20539e93e00be6101d2d592425116db6ebf5c3
Martin Højlund Therkildsen <Martin.therkildsen@gmail.com>
2021-03-08 13:06:41 +0100

    Lots of changes.
    - Revert went wrong - cant keep track of changes in this commit.

--- Simulink/constants.m
+++ Simulink/constants.m
@@ -7,10 +7,23 @@ V_g = 230;              % Grid Amplitude
 var_noise = 2;
 t_frq_step = 0.2;       % [s]
 t_phase_step = 0.3;     % [s]
-A_frq_step = 1.0;       % %-size of frequency step
-A_phase_step = 0*pi/180;  % [rad]
+A_frq_step = 1.1;       % %-size of frequency step
+A_phase_step = 20*pi/180;  % [rad]
 
 % abEPMAFPLL:
-ki = 1;
-kp = 1;
-k_phi = 1;
+%--------------
+
+fs = 10000;
+Ts = 1/fs;
+
+% MAF
+T_MAF = 0.02;
+
+
+T_st = 0.02*2; % "Fast" response time according to ali2018a p. 133
+zeta = 0.7;
+
+k_phi = T_MAF-Ts;
+ki = (4.6/(zeta*T_st))^2;
+kp = 2*zeta*sqrt(ki)+ki*k_phi;
+
