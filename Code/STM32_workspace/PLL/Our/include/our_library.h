502b9fe9fbf624493be37e700c286353495df0c2
Martin Højlund Therkildsen <Martin.therkildsen@gmail.com>
2021-04-20 13:30:18 +0200

    Added reset to ring buffer function

--- Code/STM32_workspace/PLL/Our/include/our_library.h
+++ Code/STM32_workspace/PLL/Our/include/our_library.h
@@ -1,6 +1,6 @@
 #include "CONSTANTS.h"
 float two_to_three_phase(float *phaseA, float *phaseB);
 // void circular_buffer(uint16_t bufferSize, uint16_t *circularBuffer, uint16_t *dataInput, uint8_t event, float bufSplit);
-uint8_t circular_buffer(uint16_t bufferSize, int16_t circularBuffer[][RING_BUF_SIZE], int16_t *dataInput, uint8_t event, float bufSplit, uint16_t *readStart);
+uint8_t circular_buffer(uint16_t bufferSize, int16_t circularBuffer[][RING_BUF_SIZE], int16_t *dataInput, uint8_t event, uint8_t reset, float bufSplit, uint16_t *readStart);
 
 //void printRingBuf(uint16_t bufferSize, uint16_t *circularBuffer, uint16_t readStart);
