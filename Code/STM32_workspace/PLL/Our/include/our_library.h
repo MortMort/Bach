3696d0b8455022939130d68004349df1ca1693d8
Martin Højlund Therkildsen <Martin.therkildsen@gmail.com>
2021-05-13 13:31:26 +0200

    Writing period merge commit

--- Code/STM32_workspace/PLL/Our/include/our_library.h
+++ Code/STM32_workspace/PLL/Our/include/our_library.h
@@ -2,5 +2,5 @@
 float two_to_three_phase(float *phaseA, float *phaseB);
 // void circular_buffer(uint16_t bufferSize, uint16_t *circularBuffer, uint16_t *dataInput, uint8_t event, float bufSplit);
 uint8_t circular_buffer(uint16_t bufferSize, int16_t circularBuffer[][RING_BUF_SIZE], int16_t *dataInput, uint8_t event, uint8_t reset, float bufSplit, uint16_t *readStart);
-
+float dac_offset(float var, float b, float a);
 //void printRingBuf(uint16_t bufferSize, uint16_t *circularBuffer, uint16_t readStart);
